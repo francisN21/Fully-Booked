a8e03c8ed1ce5b727aea63be1033c38663b44e22
Francisco Rones <ininorones@gmail.com>
2021-03-22 21:33:50 -0700

    New: API get and post routes working 100% tested on postman

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,7 +2,28 @@ const router = require("express").Router();
 const db = require("../models");
 
 router.get("/test", (req, res) => {
-  console.log(res.json({ msg: "hello world!" }));
+  res.send({ msg: "success" });
+});
+
+router.get("/book", (req, res) => {
+  db.Book.find({})
+    .then((dbBook) => {
+      res.json(dbBook);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.post("/book", (req, res) => {
+  const savedBook = req.body;
+  db.Book.create(savedBook)
+    .then((dbBook) => {
+      console.log(savedBook), res.json(dbBook);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 module.exports = router;
