684ef4be8969184e49e247f8c701c6b2596ec265
Francisco Rones <ininorones@gmail.com>
2021-03-22 23:55:42 -0700

    New: utils API.js created ready. onchange event created on body.js

--- client/src/Components/Body/Body.js
+++ client/src/Components/Body/Body.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import API from "../utils/API";
 import "./Body.css";
 
 const Body = () => {
+  const [userInput, setInput] = useState({
+    search: "",
+  });
+
+  const onChange = (e) => {
+    setInput({ ...userInput, [e.target.name]: e.target.value });
+  };
   return (
     <div className="container">
       <div className="row search-container">
         <div className="col-md-12 ">
-          <h1>Search Book</h1>
+          <h1>Search a book!</h1>
           <form>
             <input
               className="search-bar"
+              onChange={onChange}
               name="search"
               type="text"
               placeholder="Search book"
